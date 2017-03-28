fa271147fa642dddfba65b33360c7792e94feb92
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-03-28 18:48:56 +0000

    Fixed garbd-start.sh

--- rootfs/usr/local/bin/cluster_address.sh
+++ rootfs/usr/local/bin/cluster_address.sh
@@ -5,23 +5,19 @@ if [[ ! -z "$DEBUG" && "$DEBUG" != 0 && "${DEBUG^^}" != "FALSE" ]]; then
   set -x
 fi
 
-declare SERVICE_NAME="${SERVICE_NAME:="$(service_name.sh)"}"
-declare SERVICE_INSTANCE="${SERVICE_INSTANCE:="$(service_instance.sh)"}"
-declare NODE_ADDRESS="${NODE_ADDRESS:="$(node_address.sh)"}"
-declare CLUSTER_MINIMUM="${CLUSTER_MINIMUM:="2"}"
+source docker_info.sh
+
+declare SERVICE_NAME="${SERVICE_NAME:="$(service_name)"}"
+declare SERVICE_INSTANCE="${SERVICE_INSTANCE:="$(service_instance)"}"
+declare NODE_ADDRESS="${NODE_ADDRESS:="$(node_address)"}"
 declare CLUSTER_MEMBERS="${CLUSTER_MEMBERS:=""}"
 declare CLUSTER_ADDRESS="${CLUSTER_ADDRESS:=""}"
-declare DOCKER_HOST="${DOCKER_HOST:=""}"
-declare SEED_NODE="${SEED_NODE:=""}"
-declare SEED_NAME="${SEED_NAME:="${SERVICE_NAME/_data/_seed}"}"
+declare CLUSTER_MINIMUM="${CLUSTER_MINIMUM:="2"}"
 
-if [[ ! -z "$SEED_NODE" ]]; then
-    CLUSTER_ADDRESS="gcomm://"
-elif [[ ! -z "$CLUSTER_ADDRESS" ]]; then
+if [[ ! -z "$CLUSTER_ADDRESS" ]]; then
     ## may need to parse out members
     CLUSTER_MINIMUM=$(echo "$CLUSTER_ADDRESS" | tr ',' "\n" | sort -u | grep -v -e '^$' | wc -l)
 elif [[ ! -z "$CLUSTER_MEMBERS" ]]; then
-    CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=no"
     CLUSTER_MINIMUM=$(echo "$CLUSTER_ADDRESS" | tr ',' "\n" | sort -u | grep -v -e '^$' | wc -l)
 fi
 
@@ -29,17 +25,17 @@ fi
 # before trying to start. For example, this occurs when updated container images are being pulled
 # by `docker service update <service>` or on a full cluster power loss
 while [[ -z "$CLUSTER_ADDRESS" ]]; do
-    SEED_MEMBERS="$(getent hosts tasks.$SEED_NAME | awk -v ORS=',' '{print $1}')"
-    SERVICE_MEMBERS="$(getent hosts tasks.$SERVICE_NAME | awk -v ORS=',' '{print $1}')"
-    CLUSTER_MEMBERS="${SEED_MEMBERS}${SERVICE_MEMBERS}"
-    COUNT=$(echo CLUSTER_MEMBERS | wc -l)
+    CLUSTER_MEMBERS="$(getent hosts tasks.${SERVICE_NAME} | awk -v ORS=',' '{print $1}')"
+    COUNT=$(echo CLUSTER_MEMBERS | tr ',' ' ' | wc -w)
     if [[ $COUNT -lt $(($CLUSTER_MINIMUM)) ]]; then
         echo "Waiting for at least $CLUSTER_MINIMUM IP addresses to resolve..." >&2
         SLEEPS=$((SLEEPS + 1))
         sleep 3
+    elif [[ "${NODE_ADDDRESS}" == "${CLUSTER_MEMBERS%,*}" ]]; then
+        CLUSTER_ADDRESS="gcomm://"
     else
-        CLUSTER_MEMBERS="${CLUSTER_MEMBERS%%,}"                        # strip trailing commas
-        CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS"
+        CLUSTER_MEMBERS="${CLUSTER_MEMBERS%%,}" # strip trailing commas
+        CLUSTER_ADDRESS="gcomm://$CLUSTER_MEMBERS?pc.wait_prim=no"
     fi
 
     # After 90 seconds reduce CLUSTER_ADDRESS_MINIMUM
@@ -52,3 +48,4 @@ done
 
 echo "Cluster address set to $CLUSTER_ADDRESS" >&2
 echo $CLUSTER_ADDRESS
+
--- rootfs/usr/local/bin/garbd-start.sh
+++ rootfs/usr/local/bin/garbd-start.sh
@@ -1,6 +1,7 @@
 !#/bin/bash
 
-declare SERVICE_NAME="${SERVICE_NAME:="$(service_name.sh)"}"
-declare CLUSTER_ADDRESS="${CLUSTER_ADDRESS:="$(cluster_address.sh)"}"
+source docker_info.sh
+declare SERVICE_NAME="${SERVICE_NAME:="$(service_name)"}"
+declare CLUSTER_ADDRESS="${CLUSTER_ADDRESS:="$(cluster_address)"}"
 
 exec /usr/bin/garbd -a $CLUSTER_ADDRESS -g ${SERVICE_NAME}-cluster
