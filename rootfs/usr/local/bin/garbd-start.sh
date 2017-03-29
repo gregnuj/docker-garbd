f3bdcadfc65a880ab8d8494d8dba1be4215cbc7e
Greg Junge <gregnuj@gmail.com>
2017-03-28 19:28:04 -0500

    Update garbd-start.sh

--- rootfs/usr/local/bin/garbd-start.sh
+++ rootfs/usr/local/bin/garbd-start.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 source docker_info.sh
-declare SERVICE_NAME="${SERVICE_NAME:="$(service_name)"}"
-declare CLUSTER_ADDRESS="${CLUSTER_ADDRESS:="$(cluster_address)"}"
+declare SERVICE_NAME="$(service_name)"
+declare CLUSTER_ADDRESS="$(cluster_address)"
 
-exec /usr/bin/garbd -a $CLUSTER_ADDRESS -g ${SERVICE_NAME}-cluster
+exec "/usr/bin/garbd -a ${CLUSTER_ADDRESS} -g ${SERVICE_NAME}-cluster"
