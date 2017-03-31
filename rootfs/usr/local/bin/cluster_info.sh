168f9ba810ee23ac92d5eafe5c789bfd654356e8
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-03-31 23:48:21 +0000

    added more functions to *_info scripts

---
+++ rootfs/usr/local/bin/cluster_info.sh
@@ -0,0 +1,134 @@
+#!/bin/bash -e
+
+# Set 'DEBUG=1' environment variable to see detailed output for debugging
+if [[ ! -z "$DEBUG" && "$DEBUG" != 0 && "${DEBUG^^}" != "FALSE" ]]; then
+  set -x
+fi
+
+source docker_info.sh
+source mysql_info.sh
+
+declare GRASTATE_DAT="$(grastate_dat)"
+declare CLUSTER_NAME="$(cluster_name)"
+declare CLUSTER_USER="$(cluster_user)"
+declare CLUSTER_MEMBERS="$(cluster_members)"
+declare CLUSTER_MINIMUM="$(cluster_minimum)"
+declare CLUSTER_ADDRESS="$(cluster_address)"
+declare CLUSTER_SST_AUTH="$(cluster_sst_auth)"
+declare CLUSTER_SST_METHOD="$(cluster_sst_method)"
+declare CLUSTER_PRIMARY="$(cluster_primary)"
+
+# Defaults to servicename-cluster
+function cluster_name(){
+    CLUSTER_NAME="${CLUSTER_NAME:="$(service_name)-cluster"}"
+    echo "${CLUSTER_NAME}"
+}
+
+# Defaults to /var/lib/mysql/grastate.dat
+function grastate_dat(){
+    GRASTATE_DAT="${GRASTATE_DAT:="$(mysql_dir)/grastate.dat"}"
+    echo "${GRASTATE_DAT}"
+}
+
+# Defaults to 2
+function cluster_minimum(){
+    CLUSTER_MINIMUM="${CLUSTER_MINIMUM:="2"}"
+    echo $((CLUSTER_MINIMUM))
+}
+
+# Built from cluster members
+function cluster_address(){
+    CLUSTER_ADDRESS="${CLUSTER_ADDRESS:="gcomm://$(cluster_members)"}"
+    echo "${CLUSTER_ADDRES}"
+}
+
+#
+function cluster_user(){
+    CLUSTER_USER="${CLUSTER_USER:="xtrabackup"}"
+    echo "${CLUSTER_USER}"
+}
+
+#
+function cluster_sst_method(){
+    CLUSTER_SST_METHOD="${CLUSTER_SST_METHOD:="xtrabackup-v2"}"
+    echo "${CLUSTER_SST_METHOD}"
+}
+
+#
+function cluster_sst_auth(){
+    CLUSTER_SST_AUTH="${CLUSTER_SST_AUTH:="$(mysql_auth "$(cluster_user)")"}"
+    echo "${CLUSTER_SST_AUTH}"
+}
+
+# discovered from docker_info.SERVICE_MEMBERS using CLUSTER_MINIMUM 
+function cluster_members(){
+    while [[ -z "${CLUSTER_MEMBERS}" ]]; do
+       CURRENT_MEMBERS="$(service_members)"
+       COUNT="$(service_count)"
+       echo "Found ($COUNT) members in ${SERVICE_NAME} ($CURRENT_MEMBERS)" >&2
+       if [[ $COUNT -lt $(($CLUSTER_MINIMUM)) ]]; then
+           echo "Waiting for at least $CLUSTER_MINIMUM IP addresses to resolve..." >&2
+           SLEEPS=$((SLEEPS + 1))
+           sleep 3
+       else
+           CLUSTER_MEMBERS="$CURRENT_MEMBERS"
+       fi
+
+       # After 90 seconds reduce SERVICE_ADDRESS_MINIMUM
+       if [[ $SLEEPS -ge 30 ]]; then
+          SLEEPS=0
+          CLUSTER_MINIMUM=$((CLUSTER_MINIMUM - 1))
+          echo "Reducing CLUSTER_MINIMUM to $CLUSTER_MINIMUM" >&2
+       fi
+       if [[ $CLUSTER_MINIMUM -lt 2 ]]; then
+          echo "CLUSTER_MINIMUM is $CLUSTER_MINIMUM cannot continue" >&2
+          exit 1
+       fi
+    done
+    echo "${CLUSTER_MEMBERS}"
+}
+
+# Defaults to first node in Cluster members
+function cluster_primaryt(){
+    if [[ -z "${CLUSTER_PRIMARY}" ]]; then
+        echo "$(cluster_members)" | sort | awk -F ',' '{print $1}'
+    fi
+    echo "${CLUSTER_PRIMARY}"
+}
+
+function main(){
+    case "$1" in
+        -a|--address)
+            echo "$(cluster_address)"
+            ;;
+        --auth)
+            echo "$(cluster_sst_auth)"
+            ;;
+        -d|--dir)
+            echo "$(datadir)"
+            ;;
+        -f|--fqdn)
+            echo "$(fqdn)"
+            ;;
+        -m|--members)
+            echo "$(cluster_members)"
+            ;;
+        --method)
+            echo "$(cluster_sst_method)"
+            ;;
+        --minimum)
+            echo "$(cluster_minimum)"
+            ;;
+        -n|--name)
+            echo "$(cluster_name)"
+            ;;
+        -p|--primary)
+            echo "$(cluster_primary)"
+            ;;
+        -u|--user)
+            echo "$(cluster_user)"
+            ;;
+    esac
+}
+
+main "$@"
