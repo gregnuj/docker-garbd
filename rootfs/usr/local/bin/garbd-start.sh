8c1ef7b45a7e2c81d3fa1f9a22705e70591e698f
Junge, Gregory (gj8287) <gj8287@us.att.com>
2017-05-04 03:44:49 +0000

    Added port to gcomm address

--- rootfs/usr/local/bin/garbd-start.sh
+++ rootfs/usr/local/bin/garbd-start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -e
 
 source cluster_common.sh
-declare SERVICE_NAME="$(service_name)"
-declare CLUSTER_MEMBERS="$(cluster_members)"
-
-exec "/usr/bin/garbd" "-a" "gcomm://${CLUSTER_MEMBERS}" "-g" "${SERVICE_NAME}-cluster"
+exec "/usr/bin/garbd" "-a" "$(cluster_address)" "-g" "$(cluster_name)"
